1825a7dddd7ea3ebf36dff5fb8558c4ec612114a
Ankana-Chatterjee <90030694+Ankana-Chatterjee@users.noreply.github.com>
2023-10-13 10:45:51 +0530

    single level inheritance

--- inheritancepractice.java
+++ inheritancepractice.java
@@ -1,10 +1,26 @@
 package practice;
 
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking.");
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Dog is eating.");
+    }
+}
+
 public class inheritancepractice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.eat();  // Calls the overridden method in Dog class
+        myDog.bark();
+    }
 }
